6632ccf3d21f4548f5c7927e1136d8a9e6f17380
LeonPuchinger <lepu1590@gmail.com>
2021-02-17 14:19:50 +0100

    shh key authentication

--- src/repository.rs
+++ src/repository.rs
@@ -1,5 +1,6 @@
 use crate::error;
-use git2::{Repository};
+use dirs::home_dir;
+use git2::{Cred, FetchOptions, RemoteCallbacks, Repository};
 use std::path::Path;
 
 pub fn open_repo(path: &Path) -> Result<Repository, error::Error> {
@@ -20,6 +21,16 @@ pub fn fetch_repo(repo: &Repository) -> Result<(), error::Error> {
     }
     let mut remote = repo.find_remote(remotes.get(0).unwrap())?;
     let branch = current_branch(repo)?;
-    remote.fetch(&[branch], None, None)?;
+    let mut fetch_opt = FetchOptions::default();
+    let mut callbacks = RemoteCallbacks::default();
+    callbacks.credentials(|_url, usr_url: Option<&str>, _allowed_types| {
+        let priv_key_path = home_dir().unwrap().join(".ssh/id_rsa");
+        let mut pub_key_path = priv_key_path.to_owned();
+        pub_key_path.set_extension("pub");
+        let cred = Cred::ssh_key(usr_url.unwrap(), Some(&pub_key_path), &priv_key_path, None)?;
+        Ok(cred)
+    });
+    fetch_opt.remote_callbacks(callbacks);
+    remote.fetch(&[branch], Some(&mut fetch_opt), None)?;
     Ok(())
 }
